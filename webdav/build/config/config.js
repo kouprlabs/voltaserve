3885b071c399e1d892b91e3d323b2be328d8dbad
Elias Bouassaba <ebouassaba@gmail.com>
2023-05-22 20:54:08 +0100

    Remove build folder

--- webdav/build/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_URL = exports.IDP_URL = exports.PORT = void 0;
-exports.PORT = process.env.PORT || 6000;
-exports.IDP_URL = process.env.IDP_URL || 'http://127.0.0.1:7000';
-exports.API_URL = process.env.API_URL || 'http://127.0.0.1:5000';
-//# sourceMappingURL=config.js.map
