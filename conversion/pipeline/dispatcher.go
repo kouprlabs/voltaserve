9fde784d361422fe9d1653e3704f132c2aa990c3
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-12 09:42:15 +0100

    Refactor scheduler

--- conversion/pipeline/dispatcher.go
+++ conversion/pipeline/dispatcher.go
@@ -7,6 +7,11 @@ import (
 	"voltaserve/infra"
 )
 
+const PipelinePDF = "pdf"
+const PipelineOffice = "office"
+const PipelineImage = "image"
+const PipelineVideo = "video"
+
 type Dispatcher struct {
 	fileIdentifier *infra.FileIdentifier
 	pdfPipeline    core.Pipeline
@@ -26,27 +31,41 @@ func NewDispatcher() *Dispatcher {
 }
 
 func (svc *Dispatcher) Dispatch(opts core.PipelineOptions) error {
-	ext := filepath.Ext(opts.Key)
-	if svc.fileIdentifier.IsPDF(ext) {
+	pipeline := svc.IdentifyPipeline(opts)
+	if pipeline == PipelinePDF {
 		if err := svc.pdfPipeline.Run(opts); err != nil {
 			return err
 		}
 		return nil
-	} else if svc.fileIdentifier.IsOffice(ext) || svc.fileIdentifier.IsPlainText(ext) {
+	} else if pipeline == PipelineOffice {
 		if err := svc.officePipeline.Run(opts); err != nil {
 			return err
 		}
 		return nil
-	} else if svc.fileIdentifier.IsImage(ext) {
+	} else if pipeline == PipelineImage {
 		if err := svc.imagePipeline.Run(opts); err != nil {
 			return err
 		}
 		return nil
-	} else if svc.fileIdentifier.IsVideo(ext) {
+	} else if pipeline == PipelineVideo {
 		if err := svc.videoPipeline.Run(opts); err != nil {
 			return err
 		}
 		return nil
 	}
 	return errors.New("no matching pipeline found")
+}
+
+func (svc *Dispatcher) IdentifyPipeline(opts core.PipelineOptions) string {
+	ext := filepath.Ext(opts.Key)
+	if svc.fileIdentifier.IsPDF(ext) {
+		return PipelinePDF
+	} else if svc.fileIdentifier.IsOffice(ext) || svc.fileIdentifier.IsPlainText(ext) {
+		return PipelineOffice
+	} else if svc.fileIdentifier.IsImage(ext) {
+		return PipelineImage
+	} else if svc.fileIdentifier.IsVideo(ext) {
+		return PipelineVideo
+	}
+	return ""
 }
