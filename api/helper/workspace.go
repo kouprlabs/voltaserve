fa4296ce337c9c3ca39fcd788df874732ba9b155
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-14 11:29:10 +0100

    Code formatting, and refactoring

--- api/helper/workspace.go
+++ api/helper/workspace.go
@@ -7,11 +7,11 @@ import (
 	"github.com/gosimple/slug"
 )
 
-func WorkspaceToSlug(id string, name string) string {
+func SlugFromWorkspace(id string, name string) string {
 	return fmt.Sprintf("%s-%s", slug.Make(name), id)
 }
 
-func SlugToWorkspaceID(slug string) string {
+func WorkspaceIDFromSlug(slug string) string {
 	parts := strings.Split(slug, "-")
 	return parts[len(parts)-1]
 }
--- language/server.py
+++ language/server.py
@@ -1,24 +1,23 @@
 from flask import Flask, request, jsonify
 from iso639 import Lang
 import spacy
-import spacy_fastlang # noqa: F401 # pylint: disable=unused-import
+import spacy_fastlang  # noqa: F401 # pylint: disable=unused-import
 
 app = Flask(__name__)
 
-nlp = spacy.blank('xx')
-nlp.add_pipe('language_detector')
+nlp = spacy.blank("xx")
+nlp.add_pipe("language_detector")
 
-@app.route('/v1/health', methods=['GET'])
+
+@app.route("/v1/health", methods=["GET"])
 def health():
-  return 'OK'
+    return "OK"
 
-@app.route('/v1/detect', methods=['POST'])
+
+@app.route("/v1/detect", methods=["POST"])
 def detect():
-  content = request.json
-  text = content['text']
-  doc = nlp(text)
-  result = {
-    'language': Lang(doc._.language).pt3,
-    'score': doc._.language_score
-  }
-  return jsonify(result)
+    content = request.json
+    text = content["text"]
+    doc = nlp(text)
+    result = {"language": Lang(doc._.language).pt3, "score": doc._.language_score}
+    return jsonify(result)
