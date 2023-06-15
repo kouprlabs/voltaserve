bc9029211f00062a5b49205eea21e140037be2b7
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-15 20:01:16 +0100

    Update scripts

--- infra/start.sh
+++ infra/start.sh
@@ -4,6 +4,7 @@ BASE_DIR="/opt"
 
 start_cockroach() {
     cd "${BASE_DIR}/cockroach"
+    echo "🚀  Starting CockroachDB..."
     ./cockroach start-single-node --insecure --listen-addr=0.0.0.0:26257 --http-addr=0.0.0.0:8080 --background
 }
 
@@ -13,16 +14,19 @@ start_redis() {
 
 start_minio() {
     cd "${BASE_DIR}/minio"
+    echo "🚀  Starting MinIO..."
     MINIO_ROOT_USER=voltaserve MINIO_ROOT_PASSWORD=voltaserve nohup minio server ./data --console-address ":9001" >log.txt &
 }
 
 start_meilisearch() {
     cd "${BASE_DIR}/meilisearch"
+    echo "🚀  Starting Meilisearch..."
     nohup ./meilisearch --http-addr=0.0.0.0:7700 >log.txt &
 }
 
 start_mailhog() {
     cd "${BASE_DIR}/mailhog"
+    echo "🚀  Starting MailHog..."
     nohup ./MailHog_linux_amd64 >log.txt &
 }
 
