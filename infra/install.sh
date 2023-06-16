9acd3e9eb60d83072e440434b06523ae3fabb163
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-16 19:27:24 +0100

    Update install script

--- infra/install.sh
+++ infra/install.sh
@@ -300,7 +300,8 @@ install_air
 install_golangci
 install_swag
 
-echo "🎉 Now you are ready to develop Voltaserve! Run the command below to start the infrastrucutre sevices:"
+echo "🎉 Now you are ready to develop Voltaserve!"
+echo "Run the command below to start the infrastrucutre sevices:"
 start_cmd="curl -sSfL "https://raw.githubusercontent.com/kouprlabs/voltaserve/main/infra/start.sh?t=$(date +%s)" | sudo sh -s"
 printf "\033[36m${start_cmd}\n\e\033[0m"
 echo "To stop them, run:"
