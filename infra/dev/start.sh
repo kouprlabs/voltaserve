4a6e61437912a20dc2f3021f524cfc008d4b8101
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-16 22:51:33 +0100

    Update dev scripts

--- infra/dev/start.sh
+++ infra/dev/start.sh
@@ -15,19 +15,25 @@ start_redis() {
 start_minio() {
     cd "${BASE_DIR}/minio"
     echo "🚀  Starting MinIO..."
-    sudo sh -c MINIO_ROOT_USER=voltaserve MINIO_ROOT_PASSWORD=voltaserve 'nohup /usr/local/bin/minio server "${BASE_DIR}/minio/data" --console-address ":9001" >log.txt &'
+    local log_dir="/var/log/minio"
+    sudo mkdir -p $log_dir
+    MINIO_ROOT_USER=voltaserve MINIO_ROOT_PASSWORD=voltaserve sudo sh -c "'nohup /usr/local/bin/minio server ${BASE_DIR}/minio/data --console-address \":9001\" > ${log_dir}/log.txt 2>&1 &'"
 }
 
 start_meilisearch() {
     cd "${BASE_DIR}/meilisearch"
     echo "🚀  Starting Meilisearch..."
-    sudo sh -c 'nohup ./meilisearch --http-addr=0.0.0.0:7700 >log.txt &'
+    local log_dir="/var/log/meilisearch"
+    sudo mkdir -p $log_dir
+    sudo sh -c "'nohup ./meilisearch --http-addr=0.0.0.0:7700 > ${log_dir}/log.txt 2>&1 &'"
 }
 
 start_mailhog() {
     cd "${BASE_DIR}/mailhog"
     echo "🚀  Starting MailHog..."
-    sudo sh -c 'nohup ./MailHog_linux_amd64 >log.txt &'
+    local log_dir="/var/log/mailhog"
+    sudo mkdir -p $log_dir
+    sudo sh -c "'nohup ./MailHog_linux_amd64 > ${log_dir}/log.txt 2>&1 &'"
 }
 
 start_cockroach
