1f2dc41c3f3ab79f84d2837ef98c996d12fa814d
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-16 22:13:09 +0100

    Fix scripts

--- infra/start.sh
+++ infra/start.sh
@@ -5,29 +5,29 @@ BASE_DIR="/opt"
 start_cockroach() {
     cd "${BASE_DIR}/cockroach"
     echo "🚀  Starting CockroachDB..."
-    ./cockroach start-single-node --insecure --listen-addr=0.0.0.0:26257 --http-addr=0.0.0.0:8080 --background
+    sudo ./cockroach start-single-node --insecure --listen-addr=0.0.0.0:26257 --http-addr=0.0.0.0:8080 --background
 }
 
 start_redis() {
-    systemctl start redis
+    sudo systemctl start redis
 }
 
 start_minio() {
     cd "${BASE_DIR}/minio"
     echo "🚀  Starting MinIO..."
-    MINIO_ROOT_USER=voltaserve MINIO_ROOT_PASSWORD=voltaserve nohup /usr/local/bin/minio server "${BASE_DIR}/minio/data" --console-address ":9001" >log.txt &
+    MINIO_ROOT_USER=voltaserve MINIO_ROOT_PASSWORD=voltaserve sudo nohup /usr/local/bin/minio server "${BASE_DIR}/minio/data" --console-address ":9001" >log.txt &
 }
 
 start_meilisearch() {
     cd "${BASE_DIR}/meilisearch"
     echo "🚀  Starting Meilisearch..."
-    nohup ./meilisearch --http-addr=0.0.0.0:7700 >log.txt &
+    sudo nohup ./meilisearch --http-addr=0.0.0.0:7700 >log.txt &
 }
 
 start_mailhog() {
     cd "${BASE_DIR}/mailhog"
     echo "🚀  Starting MailHog..."
-    nohup ./MailHog_linux_amd64 >log.txt &
+    sudo nohup ./MailHog_linux_amd64 >log.txt &
 }
 
 start_cockroach
