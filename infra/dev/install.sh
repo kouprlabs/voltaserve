b458bf080c32fc6c9c0a802e441298a9053b818c
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-19 21:34:36 +0100

    Remove redundant PostgreSQL

--- infra/dev/install.sh
+++ infra/dev/install.sh
@@ -510,7 +510,6 @@ install_dnf_package "python3-pip"
 install_dnf_package "python3-devel"
 install_dnf_package "ghostscript"
 install_dnf_package "tesseract"
-install_dnf_package "postgresql"
 
 download_tesseract_trained_data "osd"
 download_tesseract_trained_data "eng"
