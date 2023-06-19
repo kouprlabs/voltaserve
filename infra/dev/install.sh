8f89a930859bee5ac2fa2b3c91d4518a67f0b554
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-19 22:07:38 +0100

    Update dev scripts

--- infra/dev/install.sh
+++ infra/dev/install.sh
@@ -302,7 +302,7 @@ install_code_ready_builder_repository() {
     fi
 }
 
-install_jbig2enc() {
+build_jbig2enc() {
     local jbig2_bin="/usr/local/bin/jbig2"
     local not_found="! (command -v $jbig2_bin >/dev/null 2>&1 && $jbig2_bin --version >/dev/null 2>&1)"
     if eval "$not_found"; then
@@ -503,14 +503,17 @@ install_mailhog
 install_minio
 install_redis
 
-install_dnf_package "golang"
-install_dnf_package "poppler-utils"
-install_dnf_package "libreoffice"
 install_dnf_package "python3-pip"
-install_dnf_package "python3-devel"
-install_dnf_package "ghostscript"
-install_dnf_package "tesseract"
+install_dnf_package "python3-devel" # Required by spacy-fastlang in language microservice
+install_pip_package "pipenv" "2023.6.12"
 
+install_dnf_package "poppler-utils" # Used by conversion microservice (pdftotext)
+install_dnf_package "libreoffice"   # Used by conversion microservice
+
+install_rpm_repository "epel" "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+install_dnf_package "GraphicsMagick" # From EPEL
+
+install_dnf_package "tesseract" # Used by conversion microservice
 download_tesseract_trained_data "osd"
 download_tesseract_trained_data "eng"
 download_tesseract_trained_data "deu"
@@ -529,24 +532,24 @@ download_tesseract_trained_data "hin"
 download_tesseract_trained_data "rus"
 download_tesseract_trained_data "ara"
 
-install_rpm_repository "epel" "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
 install_rpm_repository "rpmfusion-free-updates" "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
 install_rpm_repository "rpmfusion-nonfree-updates" "https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm"
+install_dnf_package "ffmpeg" "--allowerasing" # Used by conversion microservice. From RPM Fusion
 
-install_dnf_package "GraphicsMagick"
-install_dnf_package "pngquant"
-install_dnf_package "unpaper"
-install_dnf_package "perl-Image-ExifTool"
-install_dnf_package "dnf-plugins-core"
+# Required by OCRmyPDF
+install_dnf_package "ghostscript"
+install_dnf_package "pngquant"            # From EPEL
+install_dnf_package "unpaper"             # From EPEL
+install_dnf_package "perl-Image-ExifTool" # From EPEL
 
-install_code_ready_builder_repository
-
-install_dnf_package "ffmpeg" "--allowerasing"
+# Build tools for building jbig2enc
 install_dnf_package "automake"
 install_dnf_package "make"
 install_dnf_package "autoconf"
 install_dnf_package "libtool"
 install_dnf_package "clang"
+
+# Dependencies for building jbig2enc
 install_dnf_package "zlib"
 install_dnf_package "zlib-devel"
 install_dnf_package "libjpeg-turbo"
@@ -557,18 +560,20 @@ install_dnf_package "libtiff"
 install_dnf_package "libtiff-devel"
 install_dnf_package "libpng"
 install_dnf_package "libpng-devel"
-install_dnf_package "leptonica-devel"
+install_dnf_package "dnf-plugins-core" # Needed to enable CodeReady Builder
+install_code_ready_builder_repository
+install_dnf_package "leptonica-devel" # From CodeReady Builder
 
-install_jbig2enc
+build_jbig2enc # Required by OCRmyPDF
 
-install_pip_package "ocrmypdf" "14.2.1"
-install_pip_package "pipenv" "2023.6.12"
+install_pip_package "ocrmypdf" "14.2.1" # Used by conversion microservice
 
 install_nodejs_18
-install_corepack
+install_corepack # Needed to get PNPM
 
-install_air
-install_golangci
-install_swag
+install_dnf_package "golang"
+install_air      # Used by all Go microservices
+install_golangci # Used by all Go microservices
+install_swag     # Used by api microservice
 
 show_next_steps
