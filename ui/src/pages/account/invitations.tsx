ac89120932b654ae34200a5ddd3b09b976535e16
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-27 21:57:44 +0100

    Pagination for invitations

--- ui/src/pages/account/invitations.tsx
+++ ui/src/pages/account/invitations.tsx
@@ -1,12 +1,14 @@
 import { useCallback } from 'react'
 import {
   Center,
+  HStack,
   IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Portal,
+  Stack,
   Table,
   Tbody,
   Td,
@@ -16,20 +18,30 @@ import {
   Tr,
   useToast,
 } from '@chakra-ui/react'
-import { IconDotsVertical, SectionSpinner } from '@koupr/ui'
+import { IconDotsVertical, SectionSpinner, variables } from '@koupr/ui'
 import { Helmet } from 'react-helmet-async'
-import InvitationAPI, { Invitation } from '@/client/api/invitation'
+import InvitationAPI, { Invitation, SortOrder } from '@/client/api/invitation'
 import UserAPI from '@/client/idp/user'
 import { swrConfig } from '@/client/options'
+import PagePagination, {
+  usePagePagination,
+} from '@/components/common/page-pagination'
 
 const AccountInvitationsPage = () => {
   const toast = useToast()
   const { data: user, error: userError } = UserAPI.useGet()
+  const { page, size, onPageChange, onSizeChange } = usePagePagination({
+    localStoragePrefix: 'voltaserve',
+    localStorageNamespace: 'incoming_invitation',
+  })
   const {
-    data: invitations,
+    data: list,
     error: invitationsError,
     mutate,
-  } = InvitationAPI.useGetIncoming({}, swrConfig())
+  } = InvitationAPI.useGetIncoming(
+    { page, size, sortOrder: SortOrder.Desc },
+    swrConfig()
+  )
 
   const handleAccept = useCallback(
     async (invitationId: string) => {
@@ -60,7 +72,7 @@ const AccountInvitationsPage = () => {
   if (userError || invitationsError) {
     return null
   }
-  if (!user || !invitations) {
+  if (!user || !list) {
     return <SectionSpinner />
   }
 
@@ -69,55 +81,72 @@ const AccountInvitationsPage = () => {
       <Helmet>
         <title>{user.fullName}</title>
       </Helmet>
-      {invitations.data.length === 0 && (
+      {list.data.length === 0 && (
         <Center h="300px">
           <Text>There are no invitations.</Text>
         </Center>
       )}
-      {invitations.data.length > 0 && (
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th>From</Th>
-              <Th>Organization</Th>
-              <Th>Date</Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {invitations.data.length > 0 &&
-              invitations.data.map((e: Invitation) => (
-                <Tr key={e.id}>
-                  <Td>{e.owner.fullName}</Td>
-                  <Td>{e.organization.name}</Td>
-                  <Td>{e.updateTime || e.createTime}</Td>
-                  <Td textAlign="right">
-                    <Menu>
-                      <MenuButton
-                        as={IconButton}
-                        icon={<IconDotsVertical />}
-                        variant="ghost"
-                        aria-label=""
-                      />
-                      <Portal>
-                        <MenuList>
-                          <MenuItem onClick={() => handleAccept(e.id)}>
-                            Accept
-                          </MenuItem>
-                          <MenuItem
-                            color="red"
-                            onClick={() => handleDecline(e.id)}
-                          >
-                            Decline
-                          </MenuItem>
-                        </MenuList>
-                      </Portal>
-                    </Menu>
-                  </Td>
-                </Tr>
-              ))}
-          </Tbody>
-        </Table>
+      {list.data.length > 0 && (
+        <Stack
+          direction="column"
+          spacing={variables.spacing2Xl}
+          pb={variables.spacing2Xl}
+        >
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>From</Th>
+                <Th>Organization</Th>
+                <Th>Date</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {list.data.length > 0 &&
+                list.data.map((e: Invitation) => (
+                  <Tr key={e.id}>
+                    <Td>{e.owner.fullName}</Td>
+                    <Td>{e.organization.name}</Td>
+                    <Td>{e.updateTime || e.createTime}</Td>
+                    <Td textAlign="right">
+                      <Menu>
+                        <MenuButton
+                          as={IconButton}
+                          icon={<IconDotsVertical />}
+                          variant="ghost"
+                          aria-label=""
+                        />
+                        <Portal>
+                          <MenuList>
+                            <MenuItem onClick={() => handleAccept(e.id)}>
+                              Accept
+                            </MenuItem>
+                            <MenuItem
+                              color="red"
+                              onClick={() => handleDecline(e.id)}
+                            >
+                              Decline
+                            </MenuItem>
+                          </MenuList>
+                        </Portal>
+                      </Menu>
+                    </Td>
+                  </Tr>
+                ))}
+            </Tbody>
+          </Table>
+          {list && (
+            <HStack alignSelf="end">
+              <PagePagination
+                totalPages={list.totalPages}
+                page={page}
+                size={size}
+                onPageChange={onPageChange}
+                onSizeChange={onSizeChange}
+              />
+            </HStack>
+          )}
+        </Stack>
       )}
     </>
   )
