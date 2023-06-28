a08499192b68721332eb3b34afc73c85ead11833
Elias Bouassaba <ebouassaba@gmail.com>
2023-06-28 11:29:37 +0100

    Refactoring

--- ui/src/components/common/organization-selector.tsx
+++ ui/src/components/common/organization-selector.tsx
@@ -1,10 +1,4 @@
-import {
-  useCallback,
-  useEffect,
-  useState,
-  ChangeEvent,
-  KeyboardEvent,
-} from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import {
   Button,
   Text,
@@ -27,99 +21,16 @@ import {
   Avatar,
   Radio,
   Box,
-  InputGroup,
-  InputLeftElement,
-  Icon,
-  InputRightElement,
-  IconButton,
-  Input,
   useColorModeValue,
 } from '@chakra-ui/react'
 import { SectionSpinner, variables } from '@koupr/ui'
-import { IconClose, IconSearch } from '@koupr/ui'
 import OrganizationAPI, {
   Organization,
   SortOrder,
 } from '@/client/api/organization'
 import { swrConfig } from '@/client/options'
 import Pagination from '@/components/common/pagination'
-
-type SearchProps = {
-  query?: string
-  onChange?: (value: string) => void
-}
-
-const Search = ({ query, onChange }: SearchProps) => {
-  const [draft, setDraft] = useState('')
-  const [text, setText] = useState('')
-  const [isFocused, setIsFocused] = useState(false)
-
-  useEffect(() => {
-    if (query !== draft) {
-      setDraft(query || '')
-    }
-  }, [query, draft])
-
-  useEffect(() => {
-    onChange?.(text)
-  }, [text, onChange])
-
-  const handleClear = useCallback(() => {
-    setDraft('')
-    setText('')
-  }, [])
-
-  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setDraft(event.target.value || '')
-  }, [])
-
-  const handleSearch = useCallback((value: string) => {
-    setText(value)
-  }, [])
-
-  const handleKeyDown = useCallback(
-    (event: KeyboardEvent<HTMLInputElement>, value: string) => {
-      if (event.key === 'Enter') {
-        handleSearch(value)
-      }
-    },
-    [handleSearch]
-  )
-
-  return (
-    <HStack>
-      <InputGroup>
-        <InputLeftElement pointerEvents="none">
-          <Icon as={IconSearch} color="gray.300" />
-        </InputLeftElement>
-        <Input
-          value={draft}
-          placeholder={draft || 'Search'}
-          variant="filled"
-          onKeyDown={(event) => handleKeyDown(event, draft)}
-          onChange={handleChange}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        />
-        {draft && (
-          <InputRightElement>
-            <IconButton
-              icon={<IconClose />}
-              onClick={handleClear}
-              size="xs"
-              aria-label="Clear"
-            />
-          </InputRightElement>
-        )}
-      </InputGroup>
-      {draft || (isFocused && draft) ? (
-        <Button onClick={() => handleSearch(draft)} isDisabled={!draft}>
-          Search
-        </Button>
-      ) : null}
-    </HStack>
-  )
-}
+import SearchInput from '@/components/common/search-input'
 
 type OrganizationSelectorProps = {
   onConfirm?: (organinzation: Organization) => void
@@ -185,7 +96,10 @@ const OrganizationSelector = ({ onConfirm }: OrganizationSelectorProps) => {
           <ModalCloseButton />
           <ModalBody>
             <Stack direction="column" spacing={variables.spacing}>
-              <Search query={query} onChange={(value) => setQuery(value)} />
+              <SearchInput
+                query={query}
+                onChange={(value) => setQuery(value)}
+              />
               {!list && error && (
                 <Center h="300px">
                   <Text>Failed to load organizations.</Text>
