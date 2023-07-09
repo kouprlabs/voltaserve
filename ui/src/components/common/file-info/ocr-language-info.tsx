3c6b5ffe3a10fb71abf2bc792bbecb11a2c240c6
Elias Bouassaba <ebouassaba@gmail.com>
2023-07-09 23:11:05 +0200

    Show language name in file-info component

--- ui/src/components/common/file-info/ocr-language-info.tsx
+++ ui/src/components/common/file-info/ocr-language-info.tsx
@@ -1,20 +1,35 @@
+import { useEffect, useState } from 'react'
 import { Badge, Stat, StatLabel, StatNumber } from '@chakra-ui/react'
 import { variables } from '@koupr/ui'
 import { File } from '@/client/api/file'
+import OcrLanguageAPI from '@/client/api/ocr-language'
 
 type OcrLanguageInfoProps = {
   file: File
 }
 
 const OcrLanguageInfo = ({ file }: OcrLanguageInfoProps) => {
-  if (!file.ocr?.language) {
+  const [language, setLanguage] = useState<string>()
+
+  useEffect(() => {
+    async function fetch(language: string) {
+      const result = await OcrLanguageAPI.list({ query: language })
+      setLanguage(result.data[0].name)
+    }
+    if (file.ocr?.language) {
+      fetch(file.ocr?.language)
+    }
+  }, [file])
+
+  if (!language) {
     return null
   }
+
   return (
     <Stat>
       <StatLabel>OCR Language</StatLabel>
       <StatNumber fontSize={variables.bodyFontSize}>
-        <Badge>{file.ocr.language}</Badge>
+        <Badge>{language}</Badge>
       </StatNumber>
     </Stat>
   )
