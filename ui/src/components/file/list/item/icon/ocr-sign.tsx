1a5fb5c32380cd834ea0d5c01ec78b0976592fe0
Elias Bouassaba <ebouassaba@gmail.com>
2023-07-09 12:42:17 +0200

    Add tooltips to explain the meaning of icon badges

--- ui/src/components/file/list/item/icon/ocr-sign.tsx
+++ ui/src/components/file/list/item/icon/ocr-sign.tsx
@@ -1,16 +1,18 @@
-import { Circle } from '@chakra-ui/react'
+import { Circle, Tooltip } from '@chakra-ui/react'
 import { HiLanguage } from 'react-icons/hi2'
 
 const OcrSign = () => (
-  <Circle
-    color="MediumSpringGreen"
-    bg="white"
-    size="23px"
-    border="1px solid"
-    borderColor="gray.200"
-  >
-    <HiLanguage fontSize="14px" />
-  </Circle>
+  <Tooltip label="This item has OCR">
+    <Circle
+      color="MediumSpringGreen"
+      bg="white"
+      size="23px"
+      border="1px solid"
+      borderColor="gray.200"
+    >
+      <HiLanguage fontSize="14px" />
+    </Circle>
+  </Tooltip>
 )
 
 export default OcrSign
--- ui/src/components/file/list/item/icon/shared-sign.tsx
+++ ui/src/components/file/list/item/icon/shared-sign.tsx
@@ -1,16 +1,18 @@
-import { Circle } from '@chakra-ui/react'
+import { Circle, Tooltip } from '@chakra-ui/react'
 import { FiUsers } from 'react-icons/fi'
 
 const SharedSign = () => (
-  <Circle
-    color="darkorange"
-    bg="white"
-    size="23px"
-    border="1px solid"
-    borderColor="gray.200"
-  >
-    <FiUsers fontSize="12px" />
-  </Circle>
+  <Tooltip label="This item is shared">
+    <Circle
+      color="darkorange"
+      bg="white"
+      size="23px"
+      border="1px solid"
+      borderColor="gray.200"
+    >
+      <FiUsers fontSize="12px" />
+    </Circle>
+  </Tooltip>
 )
 
 export default SharedSign
