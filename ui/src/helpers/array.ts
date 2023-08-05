5a4efdc0d5b7b682532a354bb58359303560360b
Elias Bouassaba <ebouassaba@gmail.com>
2023-08-05 20:39:10 +0200

    chore(ui): refactoring

--- ui/src/helpers/array.ts
+++ ui/src/helpers/array.ts
@@ -4,12 +4,12 @@ export function stringArraysIdentical(a: string[], b: string[]): boolean {
   }
 
   // Sort the arrays to make sure the elements are in the same order
-  const sortedArr1 = a.slice().sort()
-  const sortedArr2 = b.slice().sort()
+  const sortedA = a.slice().sort()
+  const sortedB = b.slice().sort()
 
   // Compare each element in both arrays
-  for (let i = 0; i < sortedArr1.length; i++) {
-    if (sortedArr1[i] !== sortedArr2[i]) {
+  for (let i = 0; i < sortedA.length; i++) {
+    if (sortedA[i] !== sortedB[i]) {
       return false // Different elements, not identical
     }
   }
