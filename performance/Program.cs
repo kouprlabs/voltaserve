9d48ec072fd1c61d04d79825dd4f54e4225d0055
Elias Bouassaba <ebouassaba@gmail.com>
2024-05-24 23:13:53 +0200

    refactor: format files

--- performance/Program.cs
+++ performance/Program.cs
@@ -1,47 +1,47 @@
 ﻿namespace Defyle.WebApi
 {
-	using System.IO;
-	using Microsoft.AspNetCore;
-	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.Logging;
+  using System.IO;
+  using Microsoft.AspNetCore;
+  using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.Logging;
 
-	public class Program
-	{
-		private static IConfiguration _configuration;
+  public class Program
+  {
+    private static IConfiguration _configuration;
 
-		private static IConfiguration Configuration
-		{
-			get
-			{
-				_configuration ??= new ConfigurationBuilder()
-						.SetBasePath(Directory.GetCurrentDirectory())
-						.AddJsonFile("appsettings.json")
-						.Build();
-				return _configuration;
-			}
-		}
+    private static IConfiguration Configuration
+    {
+      get
+      {
+        _configuration ??= new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        return _configuration;
+      }
+    }
 
-		public static void Main(string[] args)
-		{
-			BuildWebHost(args).Run();
-		}
+    public static void Main(string[] args)
+    {
+      BuildWebHost(args).Run();
+    }
 
-		public static IWebHost BuildWebHost(string[] args)
-		{
-			var url = Configuration.GetValue<string>("url");
-			var multipartBodyLengthLimit = _configuration.GetValue<int>("multipartBodyLengthLimit");
+    public static IWebHost BuildWebHost(string[] args)
+    {
+      var url = Configuration.GetValue<string>("url");
+      var multipartBodyLengthLimit = _configuration.GetValue<int>("multipartBodyLengthLimit");
 
-			return WebHost.CreateDefaultBuilder(args)
-				.UseKestrel(options => { options.Limits.MaxRequestBodySize = multipartBodyLengthLimit; })
-				.UseStartup<Startup>()
-				.UseUrls(url)
-				.ConfigureLogging((webHostBuilderContext, loggingBuilder) =>
-				{
-					loggingBuilder.ClearProviders();
-					loggingBuilder.AddConsole();
-				})
-				.Build();
-		}
-	}
+      return WebHost.CreateDefaultBuilder(args)
+        .UseKestrel(options => { options.Limits.MaxRequestBodySize = multipartBodyLengthLimit; })
+        .UseStartup<Startup>()
+        .UseUrls(url)
+        .ConfigureLogging((webHostBuilderContext, loggingBuilder) =>
+        {
+          loggingBuilder.ClearProviders();
+          loggingBuilder.AddConsole();
+        })
+        .Build();
+    }
+  }
 }
