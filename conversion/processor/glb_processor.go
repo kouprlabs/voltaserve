a438f463c22f3f437233cf46b5f79de1e4ce2e88
Elias Bouassaba <ebouassaba@gmail.com>
2024-06-14 23:37:31 +0200

    chore(conversion): optimize Dockerfile using Alpine Linux

--- conversion/processor/glb_processor.go
+++ conversion/processor/glb_processor.go
@@ -2,7 +2,6 @@ package processor
 
 import (
 	"fmt"
-	"runtime"
 	"voltaserve/config"
 	"voltaserve/infra"
 )
@@ -22,15 +21,8 @@ func NewGLBProcessor() *GLBProcessor {
 }
 
 func (p *GLBProcessor) Thumbnail(inputPath string, width int, height int, color string, outputPath string) error {
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		if err := infra.NewCommand().Exec("screenshot-glb", "-i", inputPath, "-o", outputPath, "--width", fmt.Sprintf("%d", width), "--height", fmt.Sprintf("%d", height), "--color", color); err != nil {
-			return err
-		}
-	case "linux":
-		if err := infra.NewCommand().Exec("xvfb-run", "--auto-servernum", "--server-args", "-screen 0 1280x1024x24", "screenshot-glb", "-i", inputPath, "-o", outputPath, "--width", fmt.Sprintf("%d", width), "--height", fmt.Sprintf("%d", height), "--color", color); err != nil {
-			return err
-		}
+	if err := infra.NewCommand().Exec("screenshot-glb", "-i", inputPath, "-o", outputPath, "--width", fmt.Sprintf("%d", width), "--height", fmt.Sprintf("%d", height), "--color", color); err != nil {
+		return err
 	}
 	return nil
 }
