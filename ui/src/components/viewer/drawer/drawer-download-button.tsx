f1467d3e15e8e40e6a4b99cf66de8504e5929a2a
Elias Bouassaba <ebouassaba@gmail.com>
2024-06-29 01:59:09 +0200

    feat: tooltip for collapsed drawer items

--- ui/src/components/viewer/drawer/drawer-download-button.tsx
+++ ui/src/components/viewer/drawer/drawer-download-button.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton } from '@chakra-ui/react'
+import { Button, IconButton, Tooltip } from '@chakra-ui/react'
 import cx from 'classnames'
 import { File } from '@/client/api/file'
 import { IconDownload } from '@/lib/components/icons'
@@ -13,31 +13,32 @@ const DrawerDownloadButton = ({
   file,
   isCollapsed,
 }: DrawerDownloadButtonProps) => {
-  if (isCollapsed) {
-    return (
-      <IconButton
-        icon={<IconDownload />}
-        variant="solid"
-        colorScheme="blue"
-        aria-label="Download"
-        title="Download"
-        className={cx('h-[50px]', 'w-[50px]', 'p-1.5', 'rounded-md')}
-        onClick={() => downloadFile(file)}
-      />
-    )
-  } else {
-    return (
-      <Button
-        leftIcon={<IconDownload />}
-        variant="solid"
-        colorScheme="blue"
-        className={cx('h-[50px]', 'w-full', 'p-1.5', 'rounded-md')}
-        onClick={() => downloadFile(file)}
-      >
-        Download
-      </Button>
-    )
-  }
+  const label = 'Download'
+  return (
+    <Tooltip label={label} isDisabled={!isCollapsed}>
+      {isCollapsed ? (
+        <IconButton
+          icon={<IconDownload />}
+          variant="solid"
+          colorScheme="blue"
+          aria-label="Download"
+          title="Download"
+          className={cx('h-[50px]', 'w-[50px]', 'p-1.5', 'rounded-md')}
+          onClick={() => downloadFile(file)}
+        />
+      ) : (
+        <Button
+          leftIcon={<IconDownload />}
+          variant="solid"
+          colorScheme="blue"
+          className={cx('h-[50px]', 'w-full', 'p-1.5', 'rounded-md')}
+          onClick={() => downloadFile(file)}
+        >
+          {label}
+        </Button>
+      )}
+    </Tooltip>
+  )
 }
 
 export default DrawerDownloadButton
--- ui/src/lib/components/drawer/drawer-item.tsx
+++ ui/src/lib/components/drawer/drawer-item.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, useContext, useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { Tooltip } from '@chakra-ui/react'
 import cx from 'classnames'
 import { DrawerContext } from './drawer-context'
 
@@ -38,55 +39,57 @@ export const DrawerItem = ({
       title={isCollapsed ? `${primaryText}: ${secondaryText}` : secondaryText}
       className={cx('w-full')}
     >
-      <div
-        className={cx(
-          'flex',
-          'flex-row',
-          'items-center',
-          'gap-1.5',
-          'p-1.5',
-          'rounded-md',
-          {
-            'bg-black': isActive,
-            'dark:bg-white': isActive,
-          },
-          {
-            'hover:bg-gray-100': !isActive,
-            'dark:hover:bg-gray-600': !isActive,
-          },
-          {
-            'hover:bg-gray-200': !isActive,
-            'dark:hover:bg-gray-700': !isActive,
-          },
-        )}
-      >
+      <Tooltip label={primaryText} isDisabled={!isCollapsed}>
         <div
           className={cx(
             'flex',
+            'flex-row',
             'items-center',
-            'justify-center',
-            'shrink-0',
-            'w-[21px]',
-            'h-[21px]',
+            'gap-1.5',
+            'p-1.5',
+            'rounded-md',
             {
-              'text-white': isActive,
-              'dark:text-gray-800': isActive,
+              'bg-black': isActive,
+              'dark:bg-white': isActive,
+            },
+            {
+              'hover:bg-gray-100': !isActive,
+              'dark:hover:bg-gray-600': !isActive,
+            },
+            {
+              'hover:bg-gray-200': !isActive,
+              'dark:hover:bg-gray-700': !isActive,
             },
           )}
         >
-          {icon}
-        </div>
-        {!isCollapsed ? (
-          <span
-            className={cx({
-              'text-white': isActive,
-              'dark:text-gray-800': isActive,
-            })}
+          <div
+            className={cx(
+              'flex',
+              'items-center',
+              'justify-center',
+              'shrink-0',
+              'w-[21px]',
+              'h-[21px]',
+              {
+                'text-white': isActive,
+                'dark:text-gray-800': isActive,
+              },
+            )}
           >
-            {primaryText}
-          </span>
-        ) : null}
-      </div>
+            {icon}
+          </div>
+          {!isCollapsed ? (
+            <span
+              className={cx({
+                'text-white': isActive,
+                'dark:text-gray-800': isActive,
+              })}
+            >
+              {primaryText}
+            </span>
+          ) : null}
+        </div>
+      </Tooltip>
     </Link>
   )
 }
