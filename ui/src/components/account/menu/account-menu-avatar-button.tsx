dfb23cf5158e4697e2a6af7e671206ae158cd74d
Anass Bouassaba <ebouassaba@gmail.com>
2024-07-11 17:36:25 +0200

    fix(ui): show circle around active profile image when active

--- ui/src/components/account/menu/account-menu-avatar-button.tsx
+++ ui/src/components/account/menu/account-menu-avatar-button.tsx
@@ -7,7 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the GNU Affero General Public License v3.0 only, included in the file
 // licenses/AGPL.txt.
-
 import { Avatar } from '@chakra-ui/react'
 import { forwardRef } from '@chakra-ui/system'
 import cx from 'classnames'
@@ -15,8 +14,6 @@ import InvitationAPI from '@/client/api/invitation'
 import { User } from '@/client/idp/user'
 import { swrConfig } from '@/client/options'
 import NotificationBadge from '@/lib/components/notification-badge'
-import { useAppSelector } from '@/store/hook'
-import { NavType } from '@/store/ui/nav'
 import AccountMenuActiveCircle from './account-menu-active-circle'
 
 export type AccountMenuAvatarButtonProps = {
@@ -25,10 +22,7 @@ export type AccountMenuAvatarButtonProps = {
 
 const AccountMenuAvatarButton = forwardRef<AccountMenuAvatarButtonProps, 'div'>(
   ({ user, ...props }, ref) => {
-    const activeNav = useAppSelector((state) => state.ui.nav.active)
     const { data: count } = InvitationAPI.useGetIncomingCount(swrConfig())
-    const isActive = activeNav === NavType.Account
-
     return (
       <NotificationBadge hasBadge={count && count > 0 ? true : false}>
         <div ref={ref} {...props} className={cx('cursor-pointer')}>
@@ -37,11 +31,7 @@ const AccountMenuAvatarButton = forwardRef<AccountMenuAvatarButtonProps, 'div'>(
               name={user.fullName}
               src={user.picture}
               size="sm"
-              className={cx('w-[40px]', 'h-[40px]', {
-                'border': isActive,
-                'border-gray-300': isActive,
-                'dark:border-gray-700': isActive,
-              })}
+              className={cx('w-[40px]', 'h-[40px]')}
             />
           </AccountMenuActiveCircle>
         </div>
