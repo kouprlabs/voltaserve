85c9aeece2a48194458abb367f21c8e7ebfb8865
Daniël Sonck <daniel@sonck.nl>
2024-07-22 01:33:47 +0200

    feat(migration): create group table (#196)
    * feat(migration): create group table
    
    * feat(migration): create invitation table (#197)
    
    * feat(migration): create invitation table
    
    * feat(migration): create snapshot table (#198)
    
    * feat(migration): create snapshot table
    
    * feat(migration): create file tables (#199)
    
    * feat(migration): create file tables
    
    Adds the file tables and associated snapshot and permission join tables
    
    * feat(migration): create task table (#200)
    
    Adds the task table to migrations.

--- migrations/src/models/v1.rs
+++ migrations/src/models/v1.rs
@@ -1,5 +1,10 @@
+mod file;
+mod group;
+mod invitation;
 mod organization;
+mod snapshot;
 mod user;
 mod workspace;
+mod task;
 
-pub use {organization::*, user::*, workspace::*};
+pub use {file::*, group::*, invitation::*, organization::*, snapshot::*, user::*, workspace::*, task::*};
