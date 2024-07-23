8f23c60e931a64af460e916a96f461902b52b04f
Piotr Łoboda <github@lobson.pl>
2024-07-23 12:25:49 +0200

    feat(organization): base router

---
+++ admin/api/routers/organization.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+
+from admin.api.database.organization import fetch_organization, fetch_organizations
+from admin.api.models.organization import OrganizationResponse, OrganizationRequest, OrganizationListResponse, \
+    OrganizationListRequest
+
+organization_api_router = APIRouter(
+    prefix='/organization'
+)
+
+
+# --- GET --- #
+@organization_api_router.get(path="/",
+                             responses={
+                                 status.HTTP_200_OK: {
+                                     'model': OrganizationResponse
+                                 }}
+                             )
+async def get_user(data: Annotated[OrganizationRequest, Depends()]):
+    return fetch_organization(_id=data.id)
+
+
+@organization_api_router.get(path="/all",
+                             responses={
+                                 status.HTTP_200_OK: {
+                                     'model': OrganizationListResponse
+                                 }
+                             }
+                             )
+async def get_all_users(data: Annotated[OrganizationListRequest, Depends()]):
+    return fetch_organizations(page=data.page, size=data.size)
+
+# --- PATCH --- #
+
+# --- POST --- #
+
+# --- PUT --- #
+
+# --- DELETE --- #
