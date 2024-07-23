67774cf8245c195cbd19f414639d54ca2f10a30a
Piotr Łoboda <github@lobson.pl>
2024-07-23 10:51:37 +0200

    feat(organizationuser): base model

---
+++ admin/api/models/organizationuser.py
@@ -0,0 +1,26 @@
+import datetime
+from typing import List
+
+from pydantic import EmailStr
+
+from .generic import GenericPaginationRequest, GenericResponse, GenericListResponse, GenericRequest
+
+
+# --- REQUEST MODELS --- #
+class OrganizationUserRequest(GenericRequest):
+    pass
+
+
+class OrganizationUserListRequest(GenericPaginationRequest):
+    pass
+
+
+# --- RESPONSE MODELS --- #
+class OrganizationUserResponse(GenericResponse):
+    organization_id: str
+    user_id: str
+    create_time: datetime.datetime
+
+
+class OrganizationUserListResponse(GenericListResponse):
+    OrganizationUsers: List[OrganizationUserResponse]
