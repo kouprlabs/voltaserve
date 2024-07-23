4692c0224bcc380c43d696388f8655ff5819f530
Piotr Łoboda <github@lobson.pl>
2024-07-23 10:48:04 +0200

    feat(invitation): base model

---
+++ admin/api/models/invitation.py
@@ -0,0 +1,30 @@
+import datetime
+from typing import List
+
+from pydantic import EmailStr
+
+from .generic import GenericPaginationRequest, GenericResponse, GenericListResponse, GenericRequest
+
+
+# --- REQUEST MODELS --- #
+class InvitationRequest(GenericRequest):
+    pass
+
+
+class InvitationListRequest(GenericPaginationRequest):
+    pass
+
+
+# --- RESPONSE MODELS --- #
+class InvitationResponse(GenericResponse):
+    name: str
+    organization_id: str
+    owner_id: str
+    email: EmailStr
+    status: str
+    create_time: datetime.datetime
+    update_time: datetime.datetime
+
+
+class InvitationListResponse(GenericListResponse):
+    invitations: List[InvitationResponse]
