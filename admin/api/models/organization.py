5517e529df9dbf82d56cecd5be0a2359e8506bd4
Piotr Łoboda <github@lobson.pl>
2024-07-23 10:50:52 +0200

    feat(organization): base model

---
+++ admin/api/models/organization.py
@@ -0,0 +1,24 @@
+import datetime
+from typing import List
+
+from .generic import GenericPaginationRequest, GenericResponse, GenericListResponse, GenericRequest
+
+
+# --- REQUEST MODELS --- #
+class OrganizationRequest(GenericRequest):
+    pass
+
+
+class OrganizationListRequest(GenericPaginationRequest):
+    pass
+
+
+# --- RESPONSE MODELS --- #
+class OrganizationResponse(GenericResponse):
+    name: str
+    create_time: datetime.datetime
+    update_time: datetime.datetime
+
+
+class OrganizationListResponse(GenericListResponse):
+    Organizations: List[OrganizationResponse]
