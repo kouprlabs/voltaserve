af80414ce3f4f6d0e3999e5e0d0e8bca02a307a9
Piotr Łoboda <github@lobson.pl>
2024-07-23 10:34:12 +0200

    feat: base user models

---
+++ admin/api/models/user.py
@@ -0,0 +1,30 @@
+import datetime
+from typing import List
+
+from pydantic import EmailStr
+
+from .generic import GenericPaginationRequest, GenericResponse, GenericListResponse, GenericRequest
+
+
+# --- REQUEST MODELS --- #
+class UserRequest(GenericRequest):
+    pass
+
+
+class UserListRequest(GenericPaginationRequest):
+    pass
+
+
+# --- RESPONSE MODELS --- #
+class UserResponse(GenericResponse):
+    full_name: str
+    username: str
+    email: EmailStr
+    is_email_confirmed: bool
+    # picture: str
+    create_time: datetime.datetime
+    update_time: datetime.datetime
+
+
+class UserListResponse(GenericListResponse):
+    users: List[UserResponse]
