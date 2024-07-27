74dff3de9626b5159c7987517f1129fece5b39ab
Anass Bouassaba <ebouassaba@gmail.com>
2024-07-27 22:44:00 +0200

    fix(ui): linter error

--- ui/src/components/app-bar/index.tsx
+++ ui/src/components/app-bar/index.tsx
@@ -7,7 +7,6 @@
 // the Business Source License, use of this software will be governed
 // by the GNU Affero General Public License v3.0 only, included in the file
 // licenses/AGPL.txt.
-
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import cx from 'classnames'
