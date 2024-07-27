647f45fe11c574698b00c11af41bd130740f126a
Daniel Sonck <daniel@sonck.nl>
2024-07-27 12:16:24 +0200

    chore: add copyright notices to all files

--- migrations/src/m20240726_000001_normalize_schema.rs
+++ migrations/src/m20240726_000001_normalize_schema.rs
@@ -1,3 +1,12 @@
+// Copyright 2024 Daniël Sonck.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the GNU Affero General Public License v3.0 only, included in the file
+// licenses/AGPL.txt.
 use sea_orm_migration::prelude::*;
 
 use crate::models::v1::{File, GroupUser, OrganizationUser, Snapshot, SnapshotFile, Workspace};
--- migrations/src/models.rs
+++ migrations/src/models.rs
@@ -1,1 +1,10 @@
+// Copyright 2024 Daniël Sonck.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the GNU Affero General Public License v3.0 only, included in the file
+// licenses/AGPL.txt.
 pub mod v1;
