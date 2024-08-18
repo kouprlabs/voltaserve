8e7ead41401f48e53379d710f773c066c0b370c9
Piotr Łoboda <github@lobson.pl>
2024-08-18 21:11:50 +0200

    feat: missing status code

--- admin/api/errors/error_codes.py
+++ admin/api/errors/error_codes.py
@@ -12,6 +12,7 @@ from fastapi import status
 
 errors = {
     status.HTTP_500_INTERNAL_SERVER_ERROR: "internal_server_error",
+    status.HTTP_503_SERVICE_UNAVAILABLE: "service_unavailable",
     status.HTTP_400_BAD_REQUEST: "bad_request",
     status.HTTP_404_NOT_FOUND: "resource_not_found",
     status.HTTP_403_FORBIDDEN: "forbidden"
