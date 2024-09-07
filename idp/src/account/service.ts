ebeb68ad3fa0059437cbde300401c8301c5c47c8
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2024-09-07 16:12:02 +0200

    fix(idp): Remove refresh password token once it's used

--- idp/src/account/service.ts
+++ idp/src/account/service.ts
@@ -98,6 +98,7 @@ export async function resetPassword(options: AccountResetPasswordOptions) {
   await userRepo.update({
     id: user.id,
     passwordHash: hashPassword(options.newPassword),
+    resetPasswordToken: null
   })
 }
 
