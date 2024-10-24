3523eef75936fb9a02a1adc4871b66ae771053c9
Anass Bouassaba <ebouassaba@gmail.com>
2024-10-24 16:23:17 +0200

    refactor(language): better structure

--- language/api/routers/__init__.py
+++ language/api/routers/__init__.py
@@ -7,15 +7,3 @@
 # the Business Source License, use of this software will be governed
 # by the GNU Affero General Public License v3.0 only, included in the file
 # licenses/AGPL.txt.
-
-from flask import Flask
-from api.routers.entities import bp as entities_bp
-from api.routers.health import bp as health_bp
-from api.routers.version import bp as version_bp
-
-
-app = Flask(__name__)
-
-app.register_blueprint(entities_bp)
-app.register_blueprint(health_bp)
-app.register_blueprint(version_bp)
---
+++ language/api/services/entities.py
@@ -0,0 +1,113 @@
+# Copyright 2023 Anass Bouassaba.
+#
+# Use of this software is governed by the Business Source License
+# included in the file licenses/BSL.txt.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the GNU Affero General Public License v3.0 only, included in the file
+# licenses/AGPL.txt.
+
+import string
+from spacy.language import Language
+
+
+class EntityExtractor:
+    def __init__(self, nlp: Language):
+        self.nlp = nlp
+
+    def run(self, text):
+        """
+        Run the full extraction pipeline.
+
+        Args
+            text (string): A string containing text to extract entities from.
+
+        Returns:
+            list: A list of entities
+        """
+        entities = self.extract_entities(text)
+        entities = self.filter_entities(entities)
+        groups = self.group_and_count_frequency(entities)
+        return self.convert_dict_to_list(groups)
+
+    def extract_entities(self, text):
+        """
+        Extracts entities from text.
+
+        Args:
+            text (string): A string containing text to extract entities from.
+
+        Returns:
+            list: A list of entities
+        """
+        return [
+            {"text": ent.text, "label": ent.label_}
+            for doc in self.nlp.pipe([text], disable=["tagger"])
+            for sent in doc.sents
+            for ent in sent.ents
+        ]
+
+    @staticmethod
+    def filter_entities(entities):
+        """
+        Filters out entities with less than 3 characters,
+        and those with the label 'CARDINAL'.
+
+        Args:
+            entities (list): A list of entities to be filtered.
+
+        Returns:
+            list: A filtered list of entities.
+        """
+        return [
+            entity
+            for entity in entities
+            if len(entity["text"]) >= 3 and entity["label"] != "CARDINAL"
+        ]
+
+    @staticmethod
+    def group_and_count_frequency(entities):
+        """
+        Groups items by their text representation,
+        and counts the frequency of each group.
+
+        Args:
+            entities (list): A list of items to be grouped and counted.
+
+        Returns:
+            dict: A dictionary where keys are the unique text representations
+                and values are the corresponding counts.
+        """
+        result = {}
+        whitespace_and_non_printable = string.whitespace + "".join(
+            chr(i) for i in range(32)
+        )
+        for entity in entities:
+            entity["text"] = entity["text"].strip(whitespace_and_non_printable)
+            key = entity["text"].lower()
+            if key in result:
+                result[key]["frequency"] += 1
+            else:
+                result[key] = {"text": entity["text"], "frequency": 1}
+        return result
+
+    @staticmethod
+    def convert_dict_to_list(groups):
+        """
+        Convert the dictionary back to a list of entities with the "frequency" field,
+        then sort by descending order of frequency.
+
+        Args:
+            groups (dict): A dictionary where keys are entity names,
+                and values are frequencies.
+
+        Returns:
+            list: A list of tuples sorted by frequency in descending order.
+        """
+        result = [
+            {"text": value["text"], "frequency": value["frequency"]}
+            for value in groups.values()
+        ]
+        result.sort(key=lambda x: x["frequency"], reverse=True)
+        return result
