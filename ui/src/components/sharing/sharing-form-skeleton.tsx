200ef1fd43d88ad734ae307163112af1fdef23bf
Anass Bouassaba <ebouassaba@gmail.com>
2024-11-21 08:19:19 +0100

    chore(misc): refine license header and file (#384)

--- ui/src/components/sharing/sharing-form-skeleton.tsx
+++ ui/src/components/sharing/sharing-form-skeleton.tsx
@@ -1,12 +1,12 @@
-// Copyright 2023 Anass Bouassaba.
+// Copyright (c) 2023 Anass Bouassaba.
 //
 // Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
+// included in the file LICENSE in the root of this repository.
 //
 // As of the Change Date specified in that file, in accordance with
 // the Business Source License, use of this software will be governed
 // by the GNU Affero General Public License v3.0 only, included in the file
-// licenses/AGPL.txt.
+// AGPL-3.0-only in the root of this repository.
 import { Skeleton } from '@chakra-ui/react'
 import cx from 'classnames'
 
