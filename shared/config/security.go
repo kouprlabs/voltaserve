650c80d0685e9bda1989e380bb20ffc2ef9e5c3b
Anass Bouassaba <ebouassaba@gmail.com>
2025-03-01 20:50:21 +0100

    chore(shared): add security config (#521)

---
+++ shared/config/security.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Anass Bouassaba.
+//
+// Use of this software is governed by the Business Source License
+// included in the file LICENSE in the root of this repository.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the GNU Affero General Public License v3.0 only, included in the file
+// AGPL-3.0-only in the root of this repository.
+
+package config
+
+type SecurityConfig struct {
+	JWTSigningKey string
+	CORSOrigins   []string
+	APIKey        string
+}
