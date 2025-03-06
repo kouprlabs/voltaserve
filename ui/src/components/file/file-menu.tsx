ad1ea0db518c912f00ba334d46c9f300041125b0
Anass Bouassaba <ebouassaba@gmail.com>
2025-03-06 08:00:13 +0100

    wip(ui): handle file menu overflow

--- ui/src/components/file/file-menu.tsx
+++ ui/src/components/file/file-menu.tsx
@@ -188,16 +188,9 @@ const FileMenu = ({
   ])
   const isMacOS = useMemo(() => helperIsMacOS(), [])
   const [safePosition, setSafePosition] = useState<FileMenuPosition | null>()
-  // const [safeHeight, setSafeHeight] = useState<number | null>()
   const uploadInputRef = useRef<HTMLInputElement>(null)
   const menuListRef = useRef<HTMLDivElement>(null)
   const style = useMemo(() => {
-    // if (isToolbarMode && safeHeight) {
-    //   return {
-    //     height: `${safeHeight}px`,
-    //     overflowY: 'scroll',
-    //   }
-    // } else
     if (!isToolbarMode && safePosition) {
       return {
         position: 'absolute',
@@ -230,27 +223,6 @@ const FileMenu = ({
       setSafePosition(null)
     }
   }, [isOpen, menuListRef.current])
-
-  // const handleMenuButtonClick = useCallback(
-  //   (event: MouseEvent) => {
-  //     if (isToolbarMode && menuListRef.current && selection.length > 0) {
-  //       const rect = new DOMRect(
-  //         event.clientX,
-  //         event.clientY,
-  //         menuListRef.current.offsetWidth,
-  //         menuListRef.current.offsetHeight,
-  //       )
-  //       const overflow = Math.max(0, rect.bottom - window.innerHeight)
-  //       console.log(overflow, rect)
-  //       if (overflow > 0) {
-  //         setSafeHeight(rect.height - 40 - overflow)
-  //       } else {
-  //         setSafeHeight(null)
-  //       }
-  //     }
-  //   },
-  //   [isToolbarMode, selection, menuListRef.current],
-  // )
 
   const handleUploadInputChange = useCallback(
     async (event: ChangeEvent<HTMLInputElement>) => {
